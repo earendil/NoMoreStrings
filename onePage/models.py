c3554e60f755f0f853d4e3cbd821666e763eb515
lucas <lucas@debian.ld>
2017-12-26 20:50:39 +0000

    modified view model and template to read string from file

--- onePage/models.py
+++ onePage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    text = models.TextField(default='')
+
+
+class Script:
+
+    def __init__(self, ujnum=''):
+
+        self.uj = str(ujnum)
+        self.text = ''
+
+    def get_string(self):
+
+        try:
+            with open(f'/home/lucas/Desktop/uj{self.uj}.py', 'r') as f:
+                self.text = f.read()
+        except FileNotFoundError:
+            self.text = "File not found."
--- onePage/views.py
+++ onePage/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from onePage.models import Item, Script
 
 
 # Create your views here.
 def home_page(request):
+
+    if request.method == 'POST':
+
+        uj = Script(request.POST['item_text'])
+        uj.get_string()
+        # print(uj.input)
+        # print(uj.text)
+
+        # Item.objects.create(text=request.POST['item_text'])
+        # return redirect('/')
+
+        return render(request, 'index.html', {
+            'items': uj.text,
+        })
+
+    # items = Item.objects.all()
+
     return render(request, 'index.html')
